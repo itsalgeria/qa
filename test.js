8a10b851c4945a260cb244d0313538f20165b77b
itsalgeria <m.benyoub@itsolutions.dz>
2020-11-06 22:06:17 +0100

    Update test.js

--- test.js
+++ test.js
@@ -59,9 +59,7 @@ export let options = {
     max_vus: 1000,
     vus: 1000,
     stages: [
-      { duration: "30s", target: 100 },
-      { duration: "3m", target: 1000 },
-      { duration: "1m", target: 0 }
+      { duration: "5m", target: 1000 },
     ]
   }
 
