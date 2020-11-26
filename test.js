f06543c74239fe6145822202cf0844e537f2aa85
itsalgeria <m.benyoub@itsolutions.dz>
2020-11-26 22:13:33 +0100

    Update test.js

--- test.js
+++ test.js
@@ -56,10 +56,10 @@ var range = function(start, end, step) {
 }
 
 export let options = {
-    max_vus: 1000,
-    vus: 1000,
+    max_vus: 300,
+    vus: 300,
     stages: [
-      { duration: "5m", target: 1000 },
+      { duration: "3m", target: 300 },
     ]
   }
 
