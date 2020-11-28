9c20b841a1e6fbde907c77ffbd5e182d1701c2e0
itsalgeria <m.benyoub@itsolutions.dz>
2020-11-28 12:25:19 +0100

    Update test.js

--- test.js
+++ test.js
@@ -65,7 +65,7 @@ export let options = {
 
 var myRate = new Rate("error_rate");
 var myTrend = new Trend("my_trend");
-var domain = 'https://salons-masters-maroc-campusfrance.org';
+var domain = 'https://virtual.hdispo.com';
 export default function () {
     group('Login test', function(){
             var url = domain+'/event/Salon-virtuel-La-ruee-vers-lOuest-Benin-Burkina-Faso-Cote-dIvoire-Guinee-Togo-event-20/event-login';
